e8c78b41f321990b466369b1299be334302be45a
LuiS Felipe Barbarini <112336724+LFelipe19@users.noreply.github.com>
2024-03-28 13:11:22 -0300

    Update Livro.java

--- src/Model/Livro.java
+++ src/Model/Livro.java
@@ -1,2 +1,40 @@
-package Model;public class Livro {
+package Model;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private boolean disponivel;
+
+
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = true;
+    }
+
+    public void emprestar() {
+        if (disponivel == true) {
+            disponivel = false;
+        } else {
+            System.out.println("Livro Emprestado");
+        }
+
+    }
+
+    public void devolver() {
+        if (disponivel == false) {
+            disponivel = true;
+        } else {
+            System.out.println("Livro Devolvido");
+        }
+    }
+
+    public String getLivro() {
+        return titulo;
+    }
+
+    @Override
+    public String toString() {
+        return "Titulo do livro: " + titulo + "\n" + "Autor: " + autor + "\n" + "disponivel: " + disponivel + "\n";
+    }
 }
